de89074b67d4710ce976f9961b622c9b01794bf7
Jime Tutillo <93277934+JimeNayeli@users.noreply.github.com>
2026-05-10 05:25:57 -0500

    css y php arreglos

---
+++ guardar.php
@@ -0,0 +1,25 @@
+<?php
+
+$conexion = mysqli_connect(
+    "localhost",
+    "root",
+    "",
+    "zoologico"
+);
+
+if(!$conexion){
+    die("Error de conexión");
+}
+
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$mensaje = $_POST['mensaje'];
+
+$sql = "INSERT INTO contactos(nombre, correo, mensaje)
+VALUES('$nombre', '$correo', '$mensaje')";
+
+mysqli_query($conexion, $sql);
+
+echo "Datos guardados correctamente";
+
+?>
